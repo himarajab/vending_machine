1be5b99500a2c667ce06d37d1eff937b36c6efc5
himarajab <rajabhima2@gmail.com>
2022-03-25 10:37:24 +0200

    user login & logout apis

--- users/api/urls.py
+++ users/api/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
+
 app_name='users-api'
 urlpatterns = [
-    path('register', views.UserCreate.as_view())
+    path('register', views.UserCreate.as_view()),
+     path('login/', auth_views.LoginView.as_view(template_name='rest_framework/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
--- vending_machine/urls.py
+++ vending_machine/urls.py
@@ -7,7 +7,6 @@ from users.api import views
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('users-api/', include('users.api.urls', namespace='users-api'))
 ]
 
