ae2b91306ce2969e1f0585a82e21d32e216f319f
himarajab <rajabhima2@gmail.com>
2022-03-25 18:09:40 +0200

    use swagger ui

--- vending_machine/urls.py
+++ vending_machine/urls.py
@@ -1,13 +1,32 @@
+...
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path,re_path
 from users.api import views
 
+...
 
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
+
 urlpatterns = [
+   path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+   path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
-    path('users-api/', include('users.api.urls', namespace='users-api'))
+    path('users-api/', include('users.api.urls', namespace='users-api')),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
 
 
