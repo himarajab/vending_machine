a94012e50da0aa8af3e03e27f275f33753413653
himarajab <rajabhima2@gmail.com>
2022-03-25 19:25:05 +0200

    add seller field to product model

--- product/models.py
+++ product/models.py
@@ -1,11 +1,14 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+User=get_user_model()
 
 class Product(models.Model):
-    seller_id=models.IntegerField()
     amount_available=models.IntegerField()
     cost=models.DecimalField( max_digits=5, decimal_places=2)
     name=models.CharField(max_length=50)
-    
+    seller = models.ForeignKey(
+        User, on_delete=models.CASCADE
+    )
 
     class Meta:
         verbose_name = ("Product")
